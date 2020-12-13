03c3e52feb8a47b15812637bdb3186de68ced33f
adithyan-kv <adithyankv007@gmail.com>
2020-12-13 23:18:58 +0530

    Vectorized 1D random walk
     -The 1D random walk was vectorized using numpy by precomputing all the
      steps.
     -Almost an order of magnitude faster now

--- random_walks/test.py
+++ random_walks/test.py
@@ -5,27 +5,10 @@ import time
 
 
 def main():
-    x_1, y_1 = random_walk_1d(1000)
-    x_2, y_2 = random_walk_1d(1000)
-    x_3, y_3 = random_walk_1d(1000)
-    plt.plot(x_1, y_1)
-    plt.plot(x_2, y_2)
-    plt.plot(x_3, y_3)
+    for _ in range(3):
+        x, y = random_walk_1d(10000)
+        plt.plot(x, y)
     plt.show()
-
-
-@timer
-def random_walk_1d(steps):
-    x_values = []
-    y_values = []
-    steps = 10000
-    for i in range(steps):
-        x_values.append(i)
-        step = random.choice([1, -1])
-        y_values.append(step)
-    y_values = np.array(y_values)
-    y_values = np.cumsum(y_values)
-    return x_values, y_values
 
 
 def timer(func):
@@ -37,6 +20,14 @@ def timer(func):
         print(t)
         return result
     return time_it
+
+
+@ timer
+def random_walk_1d(steps):
+    x_values = np.arange(0, steps, 1)
+    step_values = np.random.choice([-1, 1], size=steps)
+    y_values = np.cumsum(step_values)
+    return x_values, y_values
 
 
 if __name__ == "__main__":
