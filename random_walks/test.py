1d62b220d26b27c0bef783f272c9f00ece2b19d3
adithyan-kv <adithyankv007@gmail.com>
2020-12-14 23:55:11 +0530

    Implemented saving as image sequence, Bug fixes
     -The images can be now saved as an image sequence showing the
      progression of the walk with time
     -Earlier the walk could take only diagonal steps, not pure left,up etc.
      That has been fixed.

--- random_walks/test.py
+++ random_walks/test.py
@@ -5,10 +5,23 @@ import time
 
 
 def main():
-    for _ in range(3):
-        x, y = random_walk_2d(10000)
-        plt.plot(x, y)
-    plt.show()
+    steps = 200
+    num_walks = 3
+    walks = []
+    for _ in range(num_walks):
+        x, y = random_walk_2d(steps)
+        walks.append([x, y])
+        colors = ['#fc8621', '#c24914', '#682c0e']
+    for i in range(steps):
+        for j in range(num_walks):
+            walk = walks[j]
+            plt.plot(walk[0][:i], walk[1][:i], color=colors[j])
+        ax = plt.axes()
+        ax.set_facecolor('#f9e0ae')
+        plt.axis('off')
+        plt.xlim(-100, 100)
+        plt.ylim(-100, 100)
+        plt.savefig(f'./test_images/walk_{i}.png')
 
 
 def timer(func):
@@ -30,8 +43,9 @@ def random_walk_1d(steps):
 
 
 def random_walk_2d(steps):
-    x_steps = np.random.choice([-1, 1], size=steps)
-    y_steps = np.random.choice([-1, 1], size=steps)
+    step_choices = [-1, 0, 1]
+    x_steps = np.random.choice(step_choices, size=steps)
+    y_steps = np.random.choice(step_choices, size=steps)
     x_values = x_steps.cumsum()
     y_values = y_steps.cumsum()
     return x_values, y_values
